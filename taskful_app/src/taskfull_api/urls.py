9f2d978871eff6d3e28d51dae320967d4502889b
hieu dinh <jrvn-hieu@Hieus-MacBook-Pro.local>
2025-03-12 17:25:04 +0700

    using viewset and router

--- taskful_app/src/taskfull_api/urls.py
+++ taskful_app/src/taskfull_api/urls.py
@@ -16,8 +16,14 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from users import router as users_api_router
+
+api_urls_patterns = [
+    path(r"accounts/", include(users_api_router.router.urls)),
+]
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include(api_urls_patterns)),
 ]
